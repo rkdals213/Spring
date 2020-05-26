0e6895879547934d9ddbb8ae3c041f07df7270a8
Kang <Kang@DESKTOP-R8OVHRU>
2020-05-26 14:15:27 +0900

    j

---
+++ YouTube_Day05/src/main/java/com/ssafy/YouTubeDay05Application.java
@@ -0,0 +1,13 @@
+package com.ssafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeDay05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTubeDay05Application.class, args);
+	}
+
+}
